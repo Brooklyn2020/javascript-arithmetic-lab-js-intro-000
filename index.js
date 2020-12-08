93f3c06054f35bdf9843f114f2260ee8584f22f5
Omar Razi <omarrazi0425@yahoo.com>
2020-12-08 18:06:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(number) {
+number ===  a + b;
+return number;
+}
+function subtract() {
+  60 - 40
+}
+function multiply() {
+  2 * 3.4
+}
+function divide() {
+  5.0 / 2.5
+}
+
+function increment(n) {
+  n++;
+  return n
+}
+
+function decrement(n) {
+  n--;
+  return n
+}
+
+function makeInt(string) {
+  string === parseInt("2", 10);
+  return string
+}
+
+function preserveDecimal(string) {
+  string === parseFloat("2.22222",10);
+  return string
+}
